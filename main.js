83285857ddf5b8c5392032f70d6a589ff3f8cdee
muaazarif3 <muaazarif3@2gmail.com>
2023-12-03 23:36:16 +0500

    correct progress bar

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 let preBtn = document.querySelector("#pre")
 let nxtBtn = document.querySelector('#nxt');
 let numberCircle = document.querySelectorAll('.number-circle');
-let counter = 1;
 let progessBlueLine = document.querySelector('.progess-blue-line');
 preBtn.disabled = true;
 
-nxtBtn.addEventListener("click", () => {
+     
+let counter = 1;
 
+
+nxtBtn.addEventListener("click", () => {
     progressWidth();
     preBtn.disabled = false;
 });
 preBtn.addEventListener("click", () => {
+    numberCircle[counter - 1].classList.remove('active');
     regressWidth();
     nxtBtn.disabled = false;
-
 })
 function progressWidth() {
+       
     if (counter < numberCircle.length) {
-        let width =  (counter * 33.33) + "%";
+        let width = (counter * 33.33) + "%";
         progessBlueLine.style.width = width;
         setTimeout(function () {
             numberCircle[counter].classList.add('active');
@@ -26,18 +29,25 @@ function progressWidth() {
     }
     if (counter >= numberCircle.length - 1) {
         nxtBtn.disabled = true;
+      
     }
+   
 }
 function regressWidth() {
+    let actives = document.querySelectorAll('.active')
     if (counter > 0) {
-        numberCircle[counter -1 ].classList.remove('active');
         counter--;
-        let width =  (counter * 33.33) + "%";
+        let width = (actives.length - 1) / (numberCircle.length - 1) * 100 + '%';
         progessBlueLine.style.width = width;
     }
 
-    if (counter <= 0) {
+    if (counter <= 1) {
         preBtn.disabled = true;
     }
+   
 
 }
+
+
+
+
